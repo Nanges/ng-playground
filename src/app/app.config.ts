8497ebfcf41b878a0f14b69a6efbf7f8986b2edc
Nanges <laha.olivier@gmail.com>
2025-03-01 22:25:09 +0100

    Setup navigation + error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, RedirectCommand, Router, withNavigationErrorHandler, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes,
+      withRouterConfig({
+        // Allow to keep full url on error
+        urlUpdateStrategy:'eager'
+      }),
+      withNavigationErrorHandler(error => {
+        const errorPath = inject(Router).parseUrl('/not-found');
+        console.error(error);
+        return new RedirectCommand(errorPath,{skipLocationChange:true});
+      })
+    )
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,36 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { ErrorComponent } from './core/error/error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+
+export const resolveContact: ResolveFn<any> = (route, state) => {
+    throw new Error('Error');
+}
 
 export const routes: Routes = [
+    {
+        path:"not-found",
+        component:NotFoundComponent
+    },
     {
         path:"home",
         component:HomeComponent
     },
     {
         path:"contact",
+        resolve:{
+            data: resolveContact
+        },
         component:ContactComponent
+    },
+    {
+        path:"",
+        redirectTo:"/home",
+        pathMatch:"full"
+    },
+    {
+        path:"**",
+        component:ErrorComponent
     }
 ];

d0d1fce5a9eae4ae32924b51c088b6745d9b15d6
Nanges <laha.olivier@gmail.com>
2025-03-01 21:01:11 +0100

    Setup basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ContactComponent } from './features/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    {
+        path:"contact",
+        component:ContactComponent
+    }
+];

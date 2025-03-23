8ab91aaca6d244196d4d4773040f7a200dc49a0c
Nanges <laha.olivier@gmail.com>
2025-03-23 17:17:35 +0100

    add scripts

--- scripts/subpackage.sh
+++ scripts/subpackage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -rf projects/$1/$2 
 mkdir projects/$1/$2
 mkdir projects/$1/$2/src
+touch projects/$1/$2/src/public_api.ts
 echo "export * from \"./public_api\"" > projects/$1/$2/src/index.ts
 echo {} > projects/$1/$2/ng-package.json
 echo Done!
